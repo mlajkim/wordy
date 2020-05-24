f476a0f23ea823fa4ad0c3b7c65ff5328de37d46
Jeongwoo Kim <jkim67cloud@gmail.com>
2020-05-24 18:53:51 +0900

    Clear out the strict mode for react

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
