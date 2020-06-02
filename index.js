0d1576c4b0b7f17f22cd0507b0affe387bd90d29
AJ Kim <jkim67cloud@gmail.com>
2020-06-02 16:05:25 +0900

    Install Morgan for the express

--- index.js
+++ index.js
@@ -2,12 +2,18 @@
 const express = require('express');
 const path = require('path');
 
+// Import the developing tools
+const morgan = require('morgan');
+
 // Initiate the express app and Export it
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connecting to Wordy
 app.use(express.static(path.join(__dirname, './client/build')));
+
+// Connecting the development tools
+app.use(morgan('dev'));
 
 // Bring the routers into the app
 const apiRouter = require('./routes/api/api');
