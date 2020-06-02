0cca603e5cd4ff34fa416c16dbacc897dadf1220
AJ Kim <jkim67cloud@gmail.com>
2020-06-02 19:42:53 +0900

    Remove no longer needed codes

--- api/api.js
+++ api/api.js
@@ -6,19 +6,6 @@ const db = new sqlite3.Database('./data.sqlite');
 // Initiate the basics
 const apiRouter = express.Router();
 
-//Hard coded word
-const word = {
-  owner_id: 1,
-  date_created: 1590297520459, // May 24, 2020 (Sun)
-  year: 2020,
-  semester: 2,
-  category_id: 2,
-  word: 'revenous',
-  pronunciation: 'reh-vuh-nus',
-  definition: 'extremely hungry',
-  example_sentence: 'I am revenous, where is my supper?'
-};
-
 // Basic returning api, now with SQL 
 apiRouter.get('/getWords', (req, res) => {
   const statement = db.prepare(`
@@ -27,7 +14,7 @@ apiRouter.get('/getWords', (req, res) => {
     LIMIT ?
   `);
 
-  statement.all([269, 5], (err, rows) => {
+  statement.all([274, 5], (err, rows) => {
     if(err) {
       throw new Error(err);
     }
