dd34a3fd3e133be43952df5102029cc746899b26
AJ Kim <jkim67cloud@gmail.com>
2020-06-08 13:31:27 +0900

    Add funtioanl routes

--- api/api.js
+++ api/api.js
@@ -6,13 +6,30 @@ const db = new sqlite3.Database('./data.sqlite');
 // Initiate the basics
 const apiRouter = express.Router();
 
+// Get all logs
+apiRouter.get('/getAllLog/:userId/:reviewType', (req, res) => {
+  const statement = db.prepare(`
+  SELECT * FROM log
+  WHERE user_id = ?
+  AND review_type = ?
+  ORDER BY word_id DESC, date_reviewed DESC
+`);
+
+statement.all([req.params.userId, req.params.reviewType], (err, row) => {
+  if(err) {
+    throw new Error(err);
+  }
+  res.json(row);
+})
+})
+
 // This api reads the last log of the user
 apiRouter.get('/getLog/:userId/:reviewType', (req, res) => {
   const statement = db.prepare(`
     SELECT * FROM log
     WHERE user_id = ?
     AND review_type = ?
-    ORDER BY date_reviewed DESC
+    ORDER BY word_id DESC, date_reviewed DESC
     LIMIT 1
   `);
 
@@ -23,6 +40,36 @@ apiRouter.get('/getLog/:userId/:reviewType', (req, res) => {
     res.json(row);
   })
 });
+
+// This api writes log
+apiRouter.get('/writeLog/:userId/:reviewType/:headWordId/:howMany', (req, res) => {
+  db.serialize(() => {
+    // Loop through according to how many times it should run
+    for(let i = 0 ; i < req.params.howMany ; i++){
+      const statement = db.prepare(`
+        INSERT INTO log (user_id, date_reviewed, review_type, word_id)
+        VALUES (?, ?, ?, ?) 
+      `);
+
+      const insertingDataArr = [
+        req.params.userId, 
+        Date.now(),
+        req.params.reviewType, 
+        (Number(req.params.headWordId) + i)
+      ];
+
+      statement.run(insertingDataArr, (err) => {
+        if(err) {
+          throw new Error(err);
+        }
+      })
+
+    }
+
+    const message = "Successful!"
+    res.json({message: message});
+  })
+})
 
 // Basic returning api, now with SQL 
 apiRouter.get('/getWords/:id', (req, res) => {
