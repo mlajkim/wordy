ad0ede8abbcaecfde52ce505223db252041e7c33
AJ Kim <jkim67cloud@gmail.com>
2020-06-08 09:27:56 +0900

    Write hello Review

---
+++ client/src/components/Review.js
@@ -0,0 +1,17 @@
+/**
+ * Review.js handles word data and logs from user to provide
+ * the correct words list for the application
+ */
+
+ // Import the necessary
+import React, {Component} from 'react';
+
+class Review extends Component {
+  render() {
+    return (
+      <div>Hellow Review!</div>
+    )
+  }
+}
+
+export default Review;
