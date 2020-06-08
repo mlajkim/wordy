6c57b243aaf63236d18e009b0a0715a424a5c686
AJ Kim <jkim67cloud@gmail.com>
2020-06-08 11:27:03 +0900

    Implement getting the log

--- api/api.js
+++ api/api.js
@@ -6,6 +6,24 @@ const db = new sqlite3.Database('./data.sqlite');
 // Initiate the basics
 const apiRouter = express.Router();
 
+// This api reads the last log of the user
+apiRouter.get('/getLog/:userId/:reviewType', (req, res) => {
+  const statement = db.prepare(`
+    SELECT * FROM log
+    WHERE user_id = ?
+    AND review_type = ?
+    ORDER BY date_reviewed DESC
+    LIMIT 1
+  `);
+
+  statement.get([req.params.userId, req.params.reviewType], (err, row) => {
+    if(err) {
+      throw new Error(err);
+    }
+    res.json(row);
+  })
+});
+
 // Basic returning api, now with SQL 
 apiRouter.get('/getWords/:id', (req, res) => {
   const statement = db.prepare(`
