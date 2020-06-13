8e85218c734fbe3d96abc033d9a4782da7dc8108
AJ Kim <jkim67cloud@gmail.com>
2020-06-13 22:07:16 +0900

    Draw the logs route

--- routes/mongoApi/logs.js
+++ routes/mongoApi/logs.js
@@ -1,9 +1,39 @@
 // Import and Declare the Router
 const logsRouter = require('express').Router();
 
-// Here
-logsRouter.get('/', (req, res) => {
-  res.send('Hello World!')
+// Import MongoDB related and its related declaratin
+const mongoose = require('mongoose')
+const yourDatabaseName = 'wordy-local'
+const url = `mongodb://127.0.0.1:27017/${yourDatabaseName}`;
+
+// Import Mongoose Models
+const logSchema = require('../../models/Log');
+
+// Connects to DB
+logsRouter.use((req, res, next) => {
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }) 
+  next();
+})
+
+// GET
+logsRouter.get('/', async (req, res) => {
+  const data = await logSchema.find()
+  res.send(data);
+})
+
+// POST
+logsRouter.post('/', (req, res) => {
+  const tempLogSchema = new logSchema({
+    userId: req.body.userId,
+    dateReviewed: req.body.dateReviewed,
+    wordId: req.body.wordId
+  })
+
+  tempWordSchema.save()
+    .then(document => console.log(document))
+    .catch(err => console.log(err))
+
+  res.send({state: 'success'});
 })
 
 // Export the router
