b3ffaecd5fe49fb928e5c29908009193808e6c26
AJ Kim <jkim67cloud@gmail.com>
2020-06-13 22:05:38 +0900

    Draw the basic user routes

--- routes/mongoApi/users.js
+++ routes/mongoApi/users.js
@@ -1,7 +1,18 @@
 // Import and Declare the Router
 const usersRouter = require('express').Router();
 
-// Here
+// Import MongoDB related and its related declaratin
+const mongoose = require('mongoose')
+const yourDatabaseName = 'wordy-local'
+const url = `mongodb://127.0.0.1:27017/${yourDatabaseName}`;
+
+// Connects to DB
+usersRouter.use((req, res, next) => {
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }) 
+  next();
+})
+
+// GET
 usersRouter.get('/', (req, res) => {
   res.send('Hello World!')
 })
