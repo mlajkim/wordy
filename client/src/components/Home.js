a54273e0f164a052ca4b2235a90055a7bfbe8c1b
AJ Kim <jkim67cloud@gmail.com>
2020-06-13 10:59:33 +0900

    Draw the basic line of parsing home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,17 +4,43 @@ import React from 'react';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
+
 class Home extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      parsetarget: ''
+    }
+    
+    this.onChange = this.onChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // Handles the change of form with the correct data!
+  onChange(e) {
+    this.setState({[e.target.name]: e.target.value})
+  }
+  
+  // Initiated when the button is clicked for the parsing!
+  handleSubmit(e){
+    const {parsetarget} = this.state;
+
+    /* fetch('/mongoApi/words', {
+      method: 'POST',
+      body: JSON.stringify(parsetarget)
+    })
+    .then(res => res.json()) */
+    
+  }
+
   render() {
     return (
-      <div>
-        <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows="3" />
-        </Form.Group>
-        <Button variant="primary">Submit for Parsing</Button>
-      </div>
-      
+      <Form.Group controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Example textarea</Form.Label>
+        <Form.Control name="parsetarget" as="textarea" rows="3" onChange={this.onChange}/>
+        <Button onClick={this.handleSubmit}>Parse it!</Button>
+      </Form.Group>      
     );
   };
 }
