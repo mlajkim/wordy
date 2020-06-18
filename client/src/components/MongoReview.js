dc48e667b7ea8d30622dedc4d924e6b54802fe4d
AJ Kim <jkim67cloud@gmail.com>
2020-06-18 20:08:18 +0900

    Practice stateless functional components

--- client/src/components/MongoReview.js
+++ client/src/components/MongoReview.js
@@ -1,25 +1,24 @@
 // Import the basics
-import React, {Component} from 'react';
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-export class MongoReview extends Component {  
-  render() {
-    return (
-      <div>
-        <Card className="text-center">
-          <Card.Header><Button variant="outline-warning" onClick={this.props.handleClickRefresh}>Refresh</Button></Card.Header>
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            {this.props.wordsNow.map(element => {
-              return <Card key={element._id} body bg="light">{element.word} [{element.pronunciation}] {element.definition} = {element.exampleSentence}</Card>
-            })}
-            <Button variant="primary" onClick={this.props.handleClickNextIndex}>Next</Button>
-          </Card.Body>
-          <Card.Footer className="text-muted">2 days ago</Card.Footer>
-        </Card>
-      </div>
-    );
-  }
+//STATELESS FUNCTIONAL COMPONENTS
+export const MongoReview = (props) => {
+  return (
+    <div>
+      <Card className="text-center">
+        <Card.Header><Button variant="outline-warning" onClick={props.handleClickRefresh}>Refresh</Button></Card.Header>
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          {props.wordsNow.map(element => {
+            return <Card key={element._id} body bg="light">{element.word} [{element.pronunciation}] {element.definition} = {element.exampleSentence}</Card>
+          })}
+          <Button variant="primary" onClick={props.handleClickNextIndex}>Next</Button>
+        </Card.Body>
+        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      </Card>
+    </div>
+  );
 }
 
