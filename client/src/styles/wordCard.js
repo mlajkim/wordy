80db23a58a54fb77a7b9a65c1a45136ad370ebeb
AJ Kim <jkim67cloud@gmail.com>
2020-06-20 16:34:32 +0900

    Create hello world for wordCard style

---
+++ client/src/styles/wordCard.js
@@ -0,0 +1,14 @@
+/**
+ * This wordCard.js contains styling of a card word
+ */
+
+// Import the basics
+import React from 'react';
+
+function wordCard () {
+  return(
+    <div>hello world</div>
+  )
+}
+
+export default wordCard;
