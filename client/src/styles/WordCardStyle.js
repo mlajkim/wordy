36f1332017a9733da688b10369c555b4e4569da2
AJ Kim <jkim67cloud@gmail.com>
2020-06-20 18:40:29 +0900

    Implement Dialog(Modal)

--- client/src/styles/WordCardStyle.js
+++ client/src/styles/WordCardStyle.js
@@ -12,6 +12,13 @@ import Badge from 'react-bootstrap/Badge';
 // Material UI Import
 import IconButton from '@material-ui/core/IconButton';
 import EditRounded from '@material-ui/icons/EditRounded';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 function StringfyWord (props) {
   const word = props.word;
@@ -44,19 +51,56 @@ function StringfyWord (props) {
 }
 
 function WordCardStyle (props) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const word = props.word;
   return(
-    <Card className="text-center" key={word.id}>
-      <Card.Body>
-        <Card.Text>
-          <StringfyWord word={word} />
-          <IconButton aria-label="edit" onClick={props.handleClickEdit}>
-            <EditRounded />
-          </IconButton>
-          <Badge style={{marginLeft: 12}} variant="success">Review</Badge>{' '}
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <div>
+      <Card className="text-center" key={word.id}>
+        <Card.Body>
+          <Card.Text>
+            <StringfyWord word={word} />
+            <IconButton aria-label="edit" onClick={handleClickOpen}>
+              <EditRounded />
+            </IconButton>
+            <Badge style={{marginLeft: 12}} variant="success">Review</Badge>{' '}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Hello World
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+    
   );
 }
 
