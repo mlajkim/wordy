eb4a593c34680920b6c25f3234b78c9ad8b0268d
AJ Kim <jkim67cloud@gmail.com>
2020-06-20 17:05:20 +0900

    Create stringfy words into the perfect format

--- client/src/components/MongoReview.js
+++ client/src/components/MongoReview.js
@@ -5,6 +5,7 @@ import Button from 'react-bootstrap/Button';
 
 // Import styles
 import LoadingAnimationStyle from '../styles/LoadingAnimationStyle'
+import WordCardStyle from '../styles/WordCardStyle';
 
 //STATELESS FUNCTIONAL COMPONENTS
 export function MongoReview (props) {
@@ -16,9 +17,7 @@ export function MongoReview (props) {
           <Card.Title>
             {props.wordsNow[0].year}-{props.wordsNow[0].semester} "{props.wordsNow[0].language}"
           </Card.Title>
-          {props.wordsNow.map(element => {
-            return <Card key={element._id} body bg="light">{element.word} [{element.pronunciation}] {element.definition} = {element.exampleSentence}</Card>
-          })}
+          {props.wordsNow.map(word => <WordCardStyle word={word}/>)}
           <Button variant="primary" onClick={props.handleClickNextIndex}>Next</Button>
         </Card.Body>
         <Card.Footer className="text-muted">2 days ago</Card.Footer>
--- client/src/styles/WordCardStyle.js
+++ client/src/styles/WordCardStyle.js
@@ -13,13 +13,43 @@ import Badge from 'react-bootstrap/Badge';
 import IconButton from '@material-ui/core/IconButton';
 import EditRounded from '@material-ui/icons/EditRounded';
 
+function StringfyWord (props) {
+  const word = props.word;
+
+  let pronunciation = '';
+  let definition = '';
+  let exampleSentence = '';
+
+  // Check if the pronunciation exists
+  if(word.pronunciation){
+    pronunciation = ` [${word.pronunciation}]`;
+  }
+
+   // Check if the pronunciation exists
+   if(word.definition){
+     // Check if the pronunciation exists again.
+     if(word.pronunciation){
+      definition = ` ${word.definition}`;
+    }else{
+      definition = ` - ${word.definition}`;
+    }
+  }
+
+  // Check if the exampleSentence exists
+  if(word.exampleSentence){
+    exampleSentence = ` = ${word.exampleSentence}`;
+  }
+
+  return `${word.word}${pronunciation}${definition}${exampleSentence}`
+}
+
 function WordCardStyle (props) {
   const word = props.word;
   return(
     <Card className="text-center" key={word.id}>
       <Card.Body>
         <Card.Text>
-          {word.word} - {word.definition}
+          <StringfyWord word={word} />
           <IconButton aria-label="edit" onClick={props.handleClickEdit}>
             <EditRounded />
           </IconButton>
