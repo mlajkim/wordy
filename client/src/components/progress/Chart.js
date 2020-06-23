ddcbbf20b4cbf493c9594e8abe8e70f0f963c872
AJ Kim <jkim67cloud@gmail.com>
2020-06-23 11:44:44 +0900

    Create sample chart for trial

--- client/src/components/progress/Chart.js
+++ client/src/components/progress/Chart.js
@@ -1,15 +1,53 @@
-import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid} from 'recharts';
+import React, { PureComponent } from 'react';
+import {
+  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
 
-export default function Chart() {
-  const data = [5, 4, 2, 3]
-  return (
-    <LineChart width={500} height={300} data={data}>
-      <XAxis dataKey="name"/>
-      <YAxis/>
-      <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
-      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-      <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
-    </LineChart>
-  );
+const data = [
+  {
+    name: 'Today', uv: 4000, pv: 2400, amt: 2400,
+  },
+  {
+    name: '22 Mon', uv: 3000, pv: 1398, amt: 2210,
+  },
+  {
+    name: '21 Sun', uv: 2000, pv: 9800, amt: 2290,
+  },
+  {
+    name: '20 Sat', uv: 2780, pv: 3908, amt: 2000,
+  },
+  {
+    name: '19 Fri', uv: 1890, pv: 4800, amt: 2181,
+  },
+  {
+    name: '18 Thu', uv: 2390, pv: 3800, amt: 2500,
+  },
+  {
+    name: '17 Wed', uv: 3490, pv: 4300, amt: 2100,
+  },
+];
+
+export default class Example extends PureComponent {
+  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/30763kr7/';
+
+  render() {
+    return (
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="pv" fill="#8884d8" />
+        <Bar dataKey="uv" fill="#82ca9d" />
+      </BarChart>
+    );
+  }
 }
