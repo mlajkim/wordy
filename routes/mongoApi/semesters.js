daa3781c3ab6fae4bd77db030da6527e1f0678f6
AJ Kim <jkim67cloud@gmail.com>
2020-06-25 07:39:04 +0900

    Make semesters get with ID

--- routes/mongoApi/semesters.js
+++ routes/mongoApi/semesters.js
@@ -1,12 +1,22 @@
 // Import and Declare the Router
 const semestersRouter = require('express').Router();
 
+// Import MongoDB related and its related declaratin
+const mongoose = require('mongoose')
+const yourDatabaseName = 'wordy-local'
+const url = `mongodb://127.0.0.1:27017/${yourDatabaseName}`;
+
 // Import Mongoose Models
 const semesterSchema = require('../../models/Semester');
 
 semestersRouter.get('/:userId', async (req, res) => {
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }) 
+
   // Find the semesterData based on the user
-  const semesterData = await semesterSchema.find({owner: req.params.userId});
+  const query = {
+    owner: req.params.userId
+  }
+  const semesterData = await semesterSchema.find(query);
 
   res.json(semesterData)
 })
