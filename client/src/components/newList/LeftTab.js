f6a89a48b3f7e8f00f3bc7579e1775a4b2a0781d
AJ Kim <jkim67cloud@gmail.com>
2020-06-24 11:59:26 +0900

    Create the upper tab design as well

--- client/src/components/newList/LeftTab.js
+++ client/src/components/newList/LeftTab.js
@@ -6,6 +6,9 @@ import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
+// Import UpperTab
+import UpperTab from './UpperTab';
+
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -44,7 +47,7 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
     display: 'flex',
-    height: 224,
+    height: 544, // Changes the length of the left tab
   },
   tabs: {
     borderRight: `1px solid ${theme.palette.divider}`,
@@ -81,7 +84,7 @@ export default function LeftTab (props) {
 
       {semesters.length !== 0 && semesters.map((semester, index) => (
           <TabPanel value={value} index={index}>
-            haha: {semester}
+            <UpperTab />
           </TabPanel>
         ))}
     </div>
--- client/src/containers/NewListContainer.js
+++ client/src/containers/NewListContainer.js
@@ -5,7 +5,7 @@ import NewList from '../components/NewList';
 export default class NewListContainer extends React.Component {
   render() {
     return (
-      <div> Hello New List  Container
+      <div> 
         <NewList />
       </div>
     )
