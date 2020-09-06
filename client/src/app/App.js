556e186e7953b34d0c322393b6ad0adf2404dc28
Jeongwoo Kim <kim@AJs-Mac-mini.local>
2020-09-06 15:11:29 +0900

    Make login feature fluid

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,5 +1,7 @@
 // Import the necessity
 import React from 'react';
+
+/*
 import {BrowserRouter , Route, Switch} from 'react-router-dom';
 import NavBar from '../components/NavBar';
 
@@ -10,75 +12,71 @@ import ListContainer from '../containers/ListContainer';
 import ProgressContainer from '../containers/ProgressContainer';
 // carefulReview
 import SignIn from '../components/SignIn';
+*/
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+// new generation
+import Appbar from '../appbar/Appbar'
+import Popup from '../popups/Popup'
 
-    // 5ee7437a908c1c3c080c4043
-    this.state = {
-      userId: '5ee7437a908c1c3c080c4043',
-      words: [],
-      
-    }
-    this.retrieveWords = this.retrieveWords.bind(this);
+const ids = ['', '5ee7437a908c1c3c080c4043'];
 
-  };
+export default function App (props) {
 
-  async retrieveWords() {
-    // Will eventually develop the below
-    // const userId = this.state.userId;
+  const [userId, setUserId] = React.useState(ids[0]);
+  const [words, setWords] = React.useState([]);
+  const [popup, setPopup] = React.useState('');
 
-    let response = await fetch('/mongoApi/words', {
-      method: 'GET',
-      headers: {'Content-Type':'application/json'}
-    })
-
-    const words = await response.json();
-    this.setState({words: words});
-
-  };
-
-  render() {
-    return (
-      <div>
-        <NavBar />
-        <BrowserRouter>
-          <Switch>
-            <Route 
-              exact path='/'
-              render={(props) => (
-                <HomeContainer {...props} retrieveWords={this.retrieveWords}/>
-              )}
-            />
-            <Route 
-              exact path='/home'
-              render={(props) => (
-                <HomeContainer {...props} retrieveWords={this.retrieveWords}/>
-              )}
-            />
-            <Route 
-              exact path='/quickReview'
-              render={(props) => (
-                <MongoReviewContainer {...props}
-                  type="quick" 
-                />
-              )}
-            />
-            <Route 
-              exact path='/carefulReview'
-              render={(props) => (
-                <MongoReviewContainer {...props}
-                  type="careful" 
-                />
-              )}
-            />
-            <Route exact path='/list' component={ListContainer} />
-            <Route exact path='/progress' component={ProgressContainer} />
-            <Route exact path='/signin' component={SignIn} />
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  };
+  return (
+    <div>
+      <Appbar userId={userId}
+              setPopup={setPopup}/>
+      <Popup userId={userId}
+             setUserId={setUserId}
+             popup={popup}
+             setPopup={setPopup}/>
+    </div>
+  );
 }
+
+/*
+let body = (
+  <div>
+    <NavBar />
+    <BrowserRouter>
+      <Switch>
+        <Route 
+          exact path='/'
+          render={(props) => (
+            <HomeContainer {...props} retrieveWords={this.retrieveWords}/>
+          )}
+        />
+        <Route 
+          exact path='/home'
+          render={(props) => (
+            <HomeContainer {...props} retrieveWords={this.retrieveWords}/>
+          )}
+        />
+        <Route 
+          exact path='/quickReview'
+          render={(props) => (
+            <MongoReviewContainer {...props}
+              type="quick" 
+            />
+          )}
+        />
+        <Route 
+          exact path='/carefulReview'
+          render={(props) => (
+            <MongoReviewContainer {...props}
+              type="careful" 
+            />
+          )}
+        />
+        <Route exact path='/list' component={ListContainer} />
+        <Route exact path='/progress' component={ProgressContainer} />
+        <Route exact path='/signin' component={SignIn} />
+      </Switch>
+    </BrowserRouter>
+  </div>
+);
+*/
---
+++ client/src/popups/Popup.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import PersonIcon from '@material-ui/icons/Person';
+import AddIcon from '@material-ui/icons/Add';
+import Typography from '@material-ui/core/Typography';
+import { blue } from '@material-ui/core/colors';
+
+const emails = ['', '5ee7437a908c1c3c080c4043'];
+const useStyles = makeStyles({
+  avatar: {
+    backgroundColor: blue[100],
+    color: blue[600],
+  },
+});
+
+function SimpleDialog(props) {
+  const classes = useStyles();
+  const { onClose, selectedValue, open } = props;
+
+  const handleClose = () => {
+    onClose(selectedValue);
+  };
+
+  const handleListItemClick = (value) => {
+    onClose(value);
+  };
+
+  return (
+    <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+      <DialogTitle id="simple-dialog-title">Choose your account</DialogTitle>
+      <List>
+        {emails.map((email) => (
+          <ListItem button onClick={() => handleListItemClick(email)} key={email}>
+            <ListItemAvatar>
+              <Avatar className={classes.avatar}>
+                <PersonIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={email} />
+          </ListItem>
+        ))}
+
+        <ListItem autoFocus button onClick={() => handleListItemClick('addAccount')}>
+          <ListItemAvatar>
+            <Avatar>
+              <AddIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Add account" />
+        </ListItem>
+      </List>
+    </Dialog>
+  );
+}
+
+SimpleDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+  selectedValue: PropTypes.string.isRequired,
+};
+
+export default function Popup(props) {
+  let open = props.popup === 'login' ? true : false;
+
+  const handleClose = (value) => {
+    props.setPopup('');
+    props.setUserId(value);
+  };
+
+  return (
+    <div>
+      <Typography variant="subtitle1">Selected: {props.userId}</Typography>
+      <br />
+      <SimpleDialog selectedValue={props.userId} open={open} onClose={handleClose} />
+    </div>
+  );
+}
