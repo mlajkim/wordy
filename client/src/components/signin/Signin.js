9e156b42b707dc91e3aa89c25b8552150a16c459
Jeongwoo Kim <kim@AJs-Mac-mini.local>
2020-09-08 14:08:22 +0900

    Add signin component

---
+++ client/src/components/signin/Signin.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Signin() {
+  return (
+    <div>
+      <img src={require('./googleSignin.png')} alt="google-sign-in"/>
+    </div>
+  );
+}
---
+++ client/src/pages/welcome/Welcome.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import
+import Signin from '../../components/signin/Signin';
+
+export default function Welcome() {
+  return (
+    <div>
+      Welcome to Wordy! 
+      You will be multi langualar easy, fast and for free
+
+      Join and become the part of our community!
+
+      <Signin />
+    </div>
+  )
+}
