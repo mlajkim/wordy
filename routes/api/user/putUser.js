d0661584da03583e4c6cba3419cd8f0b549cf260
Jeongwoo Kim <kim@AJs-Mac-mini.local>
2020-09-11 08:51:54 +0900

    Improve changing the data

--- routes/api/user/putUser.js
+++ routes/api/user/putUser.js
@@ -4,12 +4,10 @@ const userSchema = require('../../../models/User');
 
 putUserRouter.put('/:uniqueId/one/:type', async (req, res) => {
   const UNIQUE_ID = req.params.uniqueId;
-  const target = req.params.type;
-
-  console.log(target);
-  console.log(req.body.value)
+  const update = {[req.params.type]: req.body.value}
+  const option = {useFindAndModify: false}
   
-  let response = await userSchema.findByIdAndUpdate(UNIQUE_ID, {[target]: req.body.value})
+  let response = await userSchema.findByIdAndUpdate(UNIQUE_ID, update, option);
   res.send(response);
 })
 
