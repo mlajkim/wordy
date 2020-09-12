d8d9eee1161ac88f75344263ee5eca6ee2b6ef8c
Jeongwoo Kim <kim@AJs-Mac-mini.local>
2020-09-12 14:53:03 +0900

    Change in Patch Note

--- client/src/app/Patch.js
+++ client/src/app/Patch.js
@@ -3,14 +3,18 @@
  * 
  */
 
-const patch1_1 = {
-  date: 'Sep 12, 2020',
+const patch1_2 = {
+  date: 'Sep 13, 2020',
   name: 'Apricot',
-  version: '1.01',
+  version: '1.02',
   contents: [
     {
-      title: 'Now it supports HTTPS connection!',
-      explain: 'Following the industry standards for secured transfer of personal data'
+      title: 'You can securely pay through PayPal now.',
+      explain: 'Pay with PayPal securely! Set up automated payment!'
+    },
+    {
+      title: 'Minor bug fixes',
+      explain: 'Fix minor bug issues'
     }
   ],
   philosophy: [
@@ -22,20 +26,3 @@ const patch1_1 = {
     `3. 사용자들의 소중한 개인정보를 다중으로 보호한다. (프라이버시) (Inspired by Apple Inc.)`
   ]
 }
-
- const patch0_1 = {
-    date: 'Sep 10, 2020',
-    name: 'Apricot',
-    version: '1.00',
-    contents: [
-      {
-        title: 'First publish to the online',
-        explain: 'The journey has just started!'
-      }
-    ]
- }
-
- const patch1List = [patch1_1, patch0_1];
-
- export default patch1List;
-
