f9e00edb2f63f9cc760420aec0dcbfd85050cdbd
Jeongwoo Kim <jkim67cloud@gmail.com>
2020-09-27 07:17:26 +0900

    Draw concept of mogno DB

--- src/routes/api/v2/auth.ts
+++ src/routes/api/v2/auth.ts
@@ -5,22 +5,42 @@ import dotenv from "dotenv";
 const auth = express.Router();
 dotenv.config();
 
-// @POST
+// @POST: LOGIN
 auth.post("/login", (_req: Request, res: Response) => {
   const user = {
     name: "Jeongwoo"
-  }
+  };
   const accessToken = generateUserAccessToken(user);
+  const refreshToken = generateUserRefreshToken(user);
 
   res.status(200).send({
     status: 200,
     message: 'OK: Successful Login',
-    accessToken: accessToken
-  })
+    accessToken: accessToken,
+    refreshToken: refreshToken
+  });
+});
+
+// @POST: GENERATE ACCESS TOKEN FROM REFRESH TOKEN
+auth.post("/token", (req: Request, res: Response) => {
+  const refreshToken = req.body.refreshToken;
+
+  // handle when it is null
+  if(refreshToken === null) res.status(401).send({
+    status: 401,
+    message: 'NOT AUTHORIZED: REFRESH TOKEN NOT PROVIDED'
+  });
+
+
 })
 
 // @FUNCTION
 const generateUserAccessToken = (user: any) => {
+  return jwt.sign(user, process.env.LOGIN_ACCESS_TOKEN_SECRET!, {expiresIn: '60s'})
+}
+
+// @FUNCTION
+const generateUserRefreshToken = (user: any) => {
   return jwt.sign(user, process.env.LOGIN_ACCESS_TOKEN_SECRET!, {expiresIn: '60s'})
 }
 
--- src/routes/api/v2/mongo.ts
+++ src/routes/api/v2/mongo.ts
@@ -1,9 +1,10 @@
 import express, {Request, Response, NextFunction} from 'express';
 import users from './mongo/users';
 import jwt from 'jsonwebtoken';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
-// @FUNCTION
+// @ MIDDLEWARE: AUTHENTICATION
 const authenticateUser = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.header('Authorization');
   const token = authHeader && authHeader.split(' ')[1]; // undefined, or actual token
@@ -27,9 +28,20 @@ const authenticateUser = (req: Request, res: Response, next: NextFunction) => {
   next();
 };
 
+// @ MIDDLEWARE: CONNECT TO DB
+const connectToMongoDB = (_req: Request, _res: Response, next: NextFunction) => {
+  const MONGO_OWNER_NAME = process.env.MONGO_OWNER_NAME;
+  const MONGO_CLUSTER_PASSWORD = process.env.MONGO_CLUSTER_PASSWORD;
+  const MONGO_DATABASE_NAME = process.env.MONGO_DATABASE_NAME;
+  const url = `mongodb+srv://${MONGO_OWNER_NAME}:${MONGO_CLUSTER_PASSWORD}@${MONGO_DATABASE_NAME}`;
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+  next();
+};
+
 const mongo = express.Router();
-mongo.use("", authenticateUser)
-mongo.use("/users", users);
 dotenv.config();
+mongo.use(authenticateUser); // Authenticate
+mongo.use(connectToMongoDB); // Connect to DB
+mongo.use("/users", users);
 
 export default mongo;
