8172b829de14918700c886a0d5fa7a9278306305
Jeongwoo Kim <jkim67cloud@gmail.com>
2020-09-27 04:30:07 +0900

    fix a bug

--- src/routes/api/v2.ts
+++ src/routes/api/v2.ts
@@ -2,10 +2,10 @@ import express, {Request, Response} from 'express';
 
 const v2 = express.Router();
 
-v2.get("", (req: Request, res: Response) => {
-  console.log(req.body);
-  res.send({
-    message: 'welcome to v2 router'
+v2.get("", (_req: Request, res: Response) => {
+  res.status(200).send({
+    status: 200,
+    message: 'OK'
   })
 })
 
