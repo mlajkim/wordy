e827bddf81e2c5bd324533ec401c5432b820e2f0
Jeongwoo Kim <jkim67cloud@gmail.com>
2020-10-02 11:51:00 +0900

    Implement language for the first  time

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import React, {Fragment} from 'react';
 import './Home.css';
 import Button from '@material-ui/core/Button';
+import tr from './home_tr.json';
 // imgs
 import Background from '../../img/home_background.jpeg';
 // Redux
 import store from '../../redux/store';
 import {setLanguage} from '../../redux/actions';
+import {useSelector} from 'react-redux';
 
 const style = {
   intro: {
@@ -17,12 +19,17 @@ const style = {
   },
   span: {
     textDecoration: "underline", 
-    margin: 1, 
+    margin: 3, 
     cursor: "pointer"
   }
 }
+type state = {
+  dialog: string;
+  language: 'en' | 'kr';
+}
 
 const Home = () => {
+  const ln = useSelector((state: state) => state.language);
   const handleLanguageChange = () => {
     store.dispatch(setLanguage('en'));
   }
@@ -38,21 +45,13 @@ const Home = () => {
         backgroundSize: 'cover',
       }}>
         <div className="center" style={style.intro}>
-          <h1 className="center title">지금, 당신의 Multilingual Journey가 시작됩니다</h1>
-          <p>
-            Multilingual, Wordy와 함께라면 어렵지 않습니다. 
-          </p> 
-          <p>
-            다양한 나라의 사람들과 그들의 언어로 자유롭게 소통하는 것.
-          </p>
-          <p>
-            저희 Wordy는 당신의 그 꿈을 가장 빠르고 쉽게 이룰 수 있도록 돕고 있습니다. 
-          </p>
-          <p>
-            최첨단 AI를 겸비한 Wordy를 지금 무료로 이용해보세요.
-          </p>
-          <Button variant="outlined" color="primary" style={style.signUpButton}>지금 무료로 가입하기</Button>
-          <p style={{fontSize: 13}}>이미 계정이 있으시다면 <span style={style.span}>여기로</span> 로그인 해주세요</p>
+          <h1 className="center title">{tr.title[ln]}</h1>
+          <p>{tr.desc1[ln]}</p> 
+          <p>{tr.desc2[ln]}</p>
+          <p>{tr.desc3[ln]}</p>
+          <p>{tr.desc4[ln]}</p>
+          <Button variant="outlined" color="primary" style={style.signUpButton}>{tr.signUpBtn[ln]}</Button>
+          <p style={{fontSize: 13}}>{tr.loginInstead[ln]}<span style={style.span}>{tr.loginBtn[ln]}</span></p>
           <p style={{fontSize: 13}}>Wordy offered in: <span onClick={() => handleLanguageChange()}style={style.span}>English</span></p>
         </div>
       </div>
