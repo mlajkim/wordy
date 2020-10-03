f3d6c70de04ae4158c5d16fe2279c8efc4dc4fa3
Jeongwoo Kim <jkim67cloud@gmail.com>
2020-10-03 15:21:19 +0900

    Another concept of geo

--- src/routes/api/v2/ip.ts
+++ src/routes/api/v2/ip.ts
@@ -1,10 +1,14 @@
 import express, {Request, Response} from 'express';
+import geoip from 'geoip-lite';
 
 const ip = express.Router();
 
 ip.get('/location', (req: Request, res: Response) => {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   console.log(ip);
+  console.log(req.headers['x-forwarded-for'])
+  console.log(req.connection.remoteAddress)
+  console.log(geoip.lookup("211.202.88.226"));
 
   res.status(200).send({
     status: 200,
