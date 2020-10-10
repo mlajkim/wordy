a3ec78cba006d3827aecca0effb4e8e733f68706
Jeongwoo Kim <jkim67cloud@gmail.com>
2020-10-10 09:00:16 +0900

    Limitation algorithm works

--- client/src/dialog/mass_words/MassWords.tsx
+++ client/src/dialog/mass_words/MassWords.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import axios from 'axios';
 import * as API from '../../API';
 import { NewWordAddingType, State } from '../../types';
+import AvailableLangs from '../../components/available_langs/AvailableLangs';
 // Translation
 import tr from './mass_words.tr.json';
 import trAddWord from '../add_word/add_words_dialog.tr.json';
@@ -20,13 +21,34 @@ import SwitchBackToOneMode from '@material-ui/icons/AddBox';
 import store from '../../redux/store';
 import {offDialog, setNewWordAddingType, setDialog } from '../../redux/actions';
 import {useSelector} from 'react-redux';
+import ParsingAPI from './ParsingAPI';
+const LETTERS_LIMITATION = 30000
 
 const MassWords = () => {
   // Redux states
   const {user, language} = useSelector((state: State) => state);
   const ln = language;
   // Component states
+  const [count, setCount] = useState(0);
   const [massData, setMassData] = useState('');
+  const [maxError, setMaxError] = useState(false);
+
+  const handleMassDataChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setMassData(e.target.value);
+    setCount(e.target.value.length);
+    if(LETTERS_LIMITATION < e.target.value.length) {
+      setMaxError(true);
+      setMassData(e.target.value.slice(0, LETTERS_LIMITATION))
+    }else{
+      setMaxError(false);
+    }
+  }
+
+  const handleAddingMassData = () => {
+    store.dispatch(offDialog())
+    // Data check
+    ParsingAPI();
+  }
 
   return (
     <div>
@@ -49,16 +71,18 @@ const MassWords = () => {
           <DialogContentText id="alert-dialog-description">
             {tr.desc[ln]}
           </DialogContentText>
-          <TextField required id="standard-required" label="Required" 
+          <AvailableLangs />
+          <TextField required id="standard-required" label={`Data ${count}/${LETTERS_LIMITATION}`}
             style={{width: '100%', textAlign:'center'}} multiline rows={15} rowsMax={20}
-            value={massData} onChange={(e) => setMassData(e.target.value)}
+            value={massData} onChange={(e) => handleMassDataChange(e)}
+            error={maxError}
           />
         </DialogContent>
         <DialogActions>
           <Button onClick={() => store.dispatch(offDialog())} color="secondary">
             {trAddWord.btnCancel[ln]}
           </Button>
-          <Button onClick={() => store.dispatch(offDialog())} color="primary" variant="contained">
+          <Button onClick={() => handleAddingMassData()} color="primary" variant="contained">
             {trAddWord.btnOkay[ln]}
           </Button>
         </DialogActions>
---
+++ client/src/dialog/mass_words/ParsingAPI.ts
@@ -0,0 +1,9 @@
+
+
+
+//@ MAIN
+const ParsingAPI = () => {
+
+}
+
+export default ParsingAPI;
