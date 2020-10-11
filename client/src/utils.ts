1e04c961349a17774d8ccb0d71706ffb4e49869c
Jeongwoo Kim <jkim67cloud@gmail.com>
2020-10-12 05:52:39 +0900

    fix bug

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,12 +1,16 @@
 import moment from 'moment';
 
 export const format_into_sem = (year: number, semester: number) => {
-  return year % 100 + semester;
+  return (year % 100) * 10 + semester;
 };
 
 export const get_sem = () => {
   const now = moment();
-  return parseInt(now.format('YYYY')) % 100 + parseInt(now.format('MM'))
+  let sem = 1;
+  if(parseInt(now.format('MM')) >= 10) sem = 4;
+  else if (parseInt(now.format('MM')) >= 7) sem = 3;
+  else if (parseInt(now.format('MM')) >= 4) sem = 2;
+  return parseInt(now.format('YYYY')) % 100 * 10 + sem
 };
 
 export const handleCountryCode = (countryCode: string) => {
