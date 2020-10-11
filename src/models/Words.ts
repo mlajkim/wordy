503d5d6518c470189aa352b869c238bf5174bd49
Jeongwoo Kim <jkim67cloud@gmail.com>
2020-10-11 17:49:08 +0900

    Change the name

--- src/models/Words.ts
+++ src/models/Words.ts
@@ -2,17 +2,25 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const wordSchema = new Schema({
-  ownerID: String,
-  order: Number,
-  dateAdded: String,
-  sem: Number, // 204 = 2020 year of 4th semester
+  //Data wise (Personal as well) (I can calculated, how many times reviewd for each semester, does it have to be reviwed now? etc..)
+  ownerID: String, // who owns this?
+  order: Number, // the order number
+  dateAdded: String, // 204 = 2020 year of 4th semester
+  lastReviewed: String,
+  review: Array, // contains like following .. [24, 15, 30, 24] => the last 24 will be the last Review Date's semester
+  // Community Data
+  seederID: String, // who originally created this? (If owner and seeder does not match, you can edit it, but you cant sell it.)
+  packageID: String, // what kind of pacakge? (package has its own owner, selling price.. sold amounts etvc...)
+  // Personal data 
+  sem: Number,
   language: String,
   tag: Array,
   word: String,
   pronun: String,
   meaning: String,
   example: String,
-  isPublic: Boolean
+  isPublic: Boolean,
+  
 });
 
 export default mongoose.model('words', wordSchema); 
