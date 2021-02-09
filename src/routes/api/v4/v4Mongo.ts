cc847243686a638f1588d6f584b52e57860bf7ce
AJ Kim <jkim67cloud@gmail.com>
2021-02-10 08:11:55 +0900

    with auth now

--- src/routes/api/v4/v4Mongo.ts
+++ src/routes/api/v4/v4Mongo.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 // Routers
 import v4Words from './words/v4Words';
+// utils
+import { authenticateUser, connectToMongoDB } from '../../../utils';
 
 const mongo = express.Router();
 
 // Routers Apply
+mongo.use(authenticateUser);
+mongo.use(connectToMongoDB);
 mongo.use('/words', v4Words);
 
 export default mongo;
