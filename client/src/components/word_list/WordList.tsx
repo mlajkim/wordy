3453bd39cf0ce16e5ff12ab498d4e89401d9f7a4
AJ Kim <jkim67cloud@gmail.com>
2021-02-11 06:10:04 +0900

    Very small change

--- client/src/components/word_list/WordList.tsx
+++ client/src/components/word_list/WordList.tsx
@@ -6,15 +6,15 @@ type Props = { word: Word, idx: number }
 
 // @ MAIN
 const WordList: React.FC<Props> = ({ word, idx }) => {
-  let returningString: string = `${idx}. `;
+  let returningString: string = ``;
   const isPronunAvailable = word.pronun!== undefined && word.pronun.length > 0;
 
   // Making Word
   returningString += word.word// Word (Since it must exists)
-  returningString += isPronunAvailable ? ` [${word.pronun}]` : ''; // Pronun
-  returningString += typeof word.meaning!== "undefined" && word.meaning.length > 0 ? (isPronunAvailable ? ` ${word.meaning}` : `: ${word.meaning}`) : ''; // Meaning
-  returningString += typeof word.example!== "undefined" && word.example.length > 0 ? ` = ${word.example}` : ''; // Example
-  word.tag.forEach(tag => returningString += ' #' + tag )
+  returningString += isPronunAvailable ? `[${word.pronun}]` : ''; // Pronun
+  returningString += typeof word.meaning!== "undefined" && word.meaning.length > 0 ? (isPronunAvailable ? ` ${word.meaning}` : `:${word.meaning}`) : ''; // Meaning
+  returningString += typeof word.example!== "undefined" && word.example.length > 0 ? `=${word.example}` : ''; // Example
+  word.tag.forEach(tag => returningString += '#' + tag )
   
   return (
     <Fragment>
