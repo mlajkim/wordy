505a82abd30f6d2d55a951e78d247cf3e8016999
AJ Kim <jkim67cloud@gmail.com>
2021-04-01 11:04:27 +0900

    Access token now works in a better way

--- src/routes/api/v4/auth/v4AccssToken.ts
+++ src/routes/api/v4/auth/v4AccssToken.ts
@@ -3,40 +3,41 @@ const v4AccessToken = express.Router();
 // Type
 import { User } from 'src/typesBackEnd';
 // Utils
-import { connectToMongoDB, authenticateUser, generateAccessToken } from '../../../../utils';
+import { connectToMongoDB, generateAccessToken } from '../../../../utils';
 // JSON
 import response from '../../../../responseStandard.json'
 import business from '../../../../businessStandard.json'
 //
 import userSchema from '../../../../models/Users'
 
-v4AccessToken.use(authenticateUser); // Connect to DB
+// v4AccessToken.use(authenticateUser); // Connect to DB
 v4AccessToken.use(connectToMongoDB); // Connect to DB
 
-v4AccessToken.get("", async (req: Request, res: Response, next: NextFunction) => {
-  const attemptRefreshToken = req.headers.authorization;
-  const user = req.body.user;
-  const { refreshToken} = (await userSchema.findOne({ _id: user.ID }))?.toObject();
+v4AccessToken.use("", async (req: Request, res: Response, next: NextFunction) => {
+  const { refreshtoken } = req.headers;
+  const user: User = req.body.extractedUser = (await userSchema.findOne({ validRefreshToken: refreshtoken }))?.toObject();
 
-  if (attemptRefreshToken !== refreshToken) {
+  // If no such a user exists with the given refresh token, it is either expired, or invalid
+  if (!user) {
     const status = 403;
     return res.status(status).send({
       status: response[status].status,
       message: response[status].message,
-      details: "Refresh Token has been destroyed."
+      details: "Refresh Token given is either expired, not given, or incorrect."
     });
   }
   next();
 });
 
 v4AccessToken.get("", async (req: Request, res: Response) => {
-  const user: User = req.body.user;
+  const user: User = req.body.extractedUser;
   const accessToken = generateAccessToken(user);
 
   const status = 200;
   return res.status(status).send({
     status: response[status].status,
     message: response[status].message,
+    details: "Access token successfully generated",
     payload: {
       accessToken,
       expiresIn: business.V4_ACCESS_TOKEN_SECRET.expiresIn
