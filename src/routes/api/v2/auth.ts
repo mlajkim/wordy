a9f88939afb96234419dc92b9e1667efcfe1f31e
AJ Kim <jkim67cloud@gmail.com>
2021-04-30 18:31:56 +0900

    Now it never expires.

--- src/routes/api/v2/auth.ts
+++ src/routes/api/v2/auth.ts
@@ -1,10 +1,13 @@
+const IS_ACCESS_TOKEN_EXPIRABLE = false;
+const LOGIN_TOKEN_EXPIRES_IN_DAYS = 6; 
+
 import express, {Request, Response, NextFunction} from 'express';
 import jwt from 'jsonwebtoken';
 import dotenv from "dotenv";
 
 const auth = express.Router();
 dotenv.config();
-const LOGIN_TOKEN_EXPIRES_IN_DAYS = 6; 
+
 
 auth.use((req: Request, _res: Response, next: NextFunction) => {
   process.stdout.write(`[${req.body.lastName} ${req.body.lastName}] `);
@@ -20,7 +23,7 @@ auth.post("/login", (req: Request, res: Response) => {
     firstName: req.body.firstName,
     email: req.body.email
   };
-  const accessToken = generateUserAccessToken(signInAttemptingUser);
+  const accessToken = generateUserAccessToken(signInAttemptingUser, IS_ACCESS_TOKEN_EXPIRABLE);
 
   res.status(200).send({
     status: 200,
@@ -35,12 +38,20 @@ auth.post("/login", (req: Request, res: Response) => {
 });
 
 // @FUNCTION
-const generateUserAccessToken = (user: any) => {
+const generateUserAccessToken = (user: any, isExpriable: boolean) => {
+  if (isExpriable) {
+    return jwt.sign(
+      user, 
+      process.env.LOGIN_ACCESS_TOKEN_SECRET!, 
+      {expiresIn: `${LOGIN_TOKEN_EXPIRES_IN_DAYS}d`
+    }); 
+  };
+
+  // Create not expirable token
   return jwt.sign(
     user, 
-    process.env.LOGIN_ACCESS_TOKEN_SECRET!, 
-    {expiresIn: `${LOGIN_TOKEN_EXPIRES_IN_DAYS}d`
-  })
+    process.env.LOGIN_ACCESS_TOKEN_SECRET!
+  ); 
 }
 
 
