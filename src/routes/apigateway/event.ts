56418ab852663ce22d62f9d72ea9a31e23e365f4
AJ Kim <jkim67cloud@gmail.com>
2021-04-30 13:00:12 +0900

    Simple SQS style service

--- src/routes/apigateway/event.ts
+++ src/routes/apigateway/event.ts
@@ -1,9 +1,38 @@
-import express, {Request, Response} from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 
 const event = express.Router();
 
-event.get("", async (_req: Request, res: Response) => {
-  res.send('Hello World!')
+const availableEvents = [
+  {
+    eventName: "GetRefreshToken",
+    eventVersion: "2021-04-30",
+    eventAvailability: "Available",
+    unavailableDate: "" // blank for none, if eventAvailability is available, this is skipped.
+  }
+];
+
+// handles Unavailable Event
+event.use(async (req: Request, res: Response, next: NextFunction) => {
+  const filteredAvailableEvents = availableEvents.filter(availableEvent => availableEvent.eventName === req.body.eventName);
+
+  // If not found, send a response
+  if (filteredAvailableEvents.length === 0) 
+    return res.send({
+      message: "Sorry but has not found your request"
+    });
+  
+  next();
+});
+
+event.use(async (req: Request, _res: Response, next: NextFunction) => {
+  console.log(req.body);
+
+  next();
+});
+
+event.use(async (_req: Request, res: Response) => {
+  return res.send('Hello World!');
+
 });
 
 export default event;
