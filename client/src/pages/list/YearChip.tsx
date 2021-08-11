6c06ab9536bf0f450cb90aa96e243d6b5ea3dd23
AJ Kim <jkim67cloud@gmail.com>
2021-08-11 09:44:31 +0900

    go to the top of page button created

--- client/src/pages/list/YearChip.tsx
+++ client/src/pages/list/YearChip.tsx
@@ -11,12 +11,14 @@ import WordCard from '../../components/word_card/WordCard';
 import ListSetting from './ListSetting';
 import WordList from '../../components/word_list/WordList';
 // Material UI
+import Tooltip from '@material-ui/core/Tooltip';
 import Chip from '@material-ui/core/Chip';
 import Grid from '@material-ui/core/Grid'
 import CircularProgress from '@material-ui/core/CircularProgress';
 import IconButton from '@material-ui/core/IconButton';
 // Icons
 import DoneIcon from '@material-ui/icons/Done';
+import GoUpToTopPageIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 // Translation
 import tr from './year_chip.tr.json';
@@ -155,9 +157,11 @@ const YearChip = () => {
   };
 
   const renderMoreButton = (
-    <IconButton className={"ShowMoreButton"} color="inherit" aria-label="more" onClick={() => handleMoreClick()}>
-      <ExpandMoreIcon fontSize="small" style={{ color: support.isDarkMode ? buttonLight : buttonDark }} />
-    </IconButton>
+    <Tooltip title={tr.expandMore[ln]} placement="bottom">
+      <IconButton className={"ShowMoreButton"} color="inherit" aria-label="more" onClick={() => handleMoreClick()}>
+        <ExpandMoreIcon fontSize="small" style={{ color: support.isDarkMode ? buttonLight : buttonDark }} />
+      </IconButton>
+    </Tooltip>
   );
 
   // Return
@@ -216,6 +220,13 @@ const YearChip = () => {
             })
         }
       </Grid>
+      {selectedSem !== 0 && filteredWordsList &&
+        <Tooltip title={tr.toTopOfPage[ln]} placement="bottom">
+          <IconButton className={"GoToTopOfPage"} color="inherit" aria-label="more" onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}>
+            <GoUpToTopPageIcon fontSize="small" style={{ color: support.isDarkMode ? buttonLight : buttonDark }} />
+          </IconButton>
+        </Tooltip> 
+      }
       { selectedSem !== 0 && typeof filteredWordsList !== "boolean" && wordCardsMax < filteredWordsList.length! && renderMoreButton }
     </Fragment>
   );
