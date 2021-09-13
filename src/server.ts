6ed9c290c25b6fa7ead79a8e54655015d2f86abf
AJ Kim <jkim67cloud@gmail.com>
2021-09-13 12:39:56 +0900

    Added new certificate!

--- src/server.ts
+++ src/server.ts
@@ -24,18 +24,29 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api', api); //REST API
 app.use('/apigateway', apigateway); //REST API
-app.use('/graphql', graphqlHTTP({schema, graphiql: true}));  //GraphQL
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));  //GraphQL
 app.get('*', (_req: Request, res: Response) => {
   res.sendFile('index.html', {root: path.join(__dirname, '../client/build/')});
 });
 
-// https certificates import
+// 2020-2021 OPTION
+/*
 const option = {
   ca: fs.readFileSync('./certificates/ca-bundle.crt'), // Certificate BUNDLES
   cert: fs.readFileSync('./certificates/wordy-cloud_com.crt'), // correct
   key: fs.readFileSync('./certificates/comodo-ssl-ca.key'), // mostliekly correct  
-  passphrase: process.env.COMODO_SSL_KEY_PASSWORD, // password for the key
+  passphrase: process.env.COMODO_SSL_KEY_PASSWORD_2020, // password for the key
 };
+*/
+
+// Certificate Path === CP
+const CP = './certificates/2021/';
+const option = {
+  ca: fs.readFileSync(`${CP}My_CA_Bundle.ca-bundle.crt`), // Certificate BUNDLES
+  cert: fs.readFileSync(`${CP}www_wordy-cloud_com.crt`), // correct
+  key: fs.readFileSync(`${CP}comodo-ssl-key-2021.key`), // mostliekly correct  
+  passphrase: process.env.COMODO_SSL_KEY_PASSWORD_2021, // password for the key
+}
 
 // Write the time today
 console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
