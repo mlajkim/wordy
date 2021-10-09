b123fcd0705df6f73a8602c6a291f0b78ed4a127
AJ Kim <jkim67cloud@gmail.com>
2021-10-09 21:19:49 +0900

    Fixed bug. forgot #... holy cow

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -4,26 +4,21 @@
  */
 
 const WHITE = "#FFF"
-const BLACK = "000"
+const BLACK = "#000"
+const APEX_DARK_GRAY = "#2b3035"
 
 // Background page
-export const backgroundLight = WHITE;
-export const backgroundDark = '#2b3035';
+export const backgroundLight = WHITE
+export const backgroundDark = APEX_DARK_GRAY
 
-export const buttonColorNonHover = "#B5B5B5";
-export const buttonColorHover = "#BDFBA3";
+export const buttonColorNonHover = "#B5B5B5"
+export const buttonColorHover = "#BDFBA3"
 
-// Font Color
-// Comment on Jul 18, 2021 below
-/**
- * Just realized that fontLight should be #FFF for better readaiblity.
- * Yet since this is already depedent on many application I will ignore for now. 
- */
- export const fontDark = WHITE;
-export const fontLight = BLACK;
+export const fontDark = WHITE
+export const fontLight = BLACK
 
-export const buttonLight = BLACK;
-export const buttonDark = "#B5B5B5"; // #FFF is too standing
+export const buttonDark = "#B5B5B5" // #FFF is too standing
+export const buttonLight = BLACK
 
 // ! October, 2021
 export const encryptedButtonDark = "#87CF8D"
@@ -35,14 +30,14 @@ export const unencryptedButtonLight = "#FF4E17"
 
 
 // Appbar
-export const appbarLight = '#ccff99';
-export const appbarDark = '#59b300';
+export const appbarLight = '#ccff99'
+export const appbarDark = '#59b300'
 export const appbarDevMode = '#C22656'
 
 // Word List Backgroud Color
-export const listLight = "#F2F2F2";
-export const listDark = "#333333";
+export const listLight = "#F2F2F2"
+export const listDark = "#333333"
 
 // Word Card Color
-export const wordCardLight = WHITE;
-export const wordCardDark = "#404040";
+export const wordCardLight = WHITE
+export const wordCardDark = "#404040"
